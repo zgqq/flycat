64905a73d21753e2d13e671293bed5d5d5d10ada
zgqq <zgqjava@gmail.com>
2019-09-23 12:11:54 +0800

    chore(alarm): limit count per second

--- flycat-alarm/flycat-alarm-spi/src/main/java/com/github/flycat/spi/alarm/AbstractAlarmSender.java
+++ flycat-alarm/flycat-alarm-spi/src/main/java/com/github/flycat/spi/alarm/AbstractAlarmSender.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2019 zgqq <zgqjava@gmail.com>
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -17,21 +17,28 @@ package com.github.flycat.spi.alarm;
 
 import com.codahale.metrics.Meter;
 import com.codahale.metrics.MetricRegistry;
+import com.google.common.util.concurrent.RateLimiter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public abstract class AbstractAlarmSender implements AlarmSender {
     private static final Logger LOGGER = LoggerFactory.getLogger(AbstractAlarmSender.class);
     private static final MetricRegistry REGISTRY = new MetricRegistry();
+    private static final RateLimiter rateLimiter = RateLimiter.create(1.0);
 
     @Override
     public void sendNotify(String message) {
-        final Meter meter = REGISTRY.meter("log." + message);
-        meter.mark();
-        if (meter.getOneMinuteRate() < 0.4) {
-            doSendNotify(message);
+        final boolean require = rateLimiter.tryAcquire();
+        if (require) {
+            final Meter meter = REGISTRY.meter("log." + message);
+            meter.mark();
+            if (meter.getOneMinuteRate() < 0.4) {
+                doSendNotify(message);
+            } else {
+                LOGGER.warn("Alarm too frequently, aborted, message:{}", message);
+            }
         } else {
-            LOGGER.warn("Alarm too frequently, aborted, message:{}", message);
+            LOGGER.info("Unable to get token, abort alarm, message:{}", message);
         }
     }
 
