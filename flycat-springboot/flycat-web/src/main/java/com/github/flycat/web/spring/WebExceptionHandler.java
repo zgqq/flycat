dcca7c09ef8909e03dfa2a445a26e65002e11024
zgqq <zgqjava@gmail.com>
2019-10-16 22:52:12 +0800

    fix(springboot): fix ClassCastException

--- flycat-springboot/flycat-web/src/main/java/com/github/flycat/web/spring/WebExceptionHandler.java
+++ flycat-springboot/flycat-web/src/main/java/com/github/flycat/web/spring/WebExceptionHandler.java
@@ -1,12 +1,12 @@
 /**
  * Copyright 2019 zgqq <zgqjava@gmail.com>
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -30,6 +30,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import javax.validation.ValidationException;
@@ -90,7 +91,19 @@ public class WebExceptionHandler {
     @ResponseBody
     public Object handleApiException(Throwable ex, HttpServletRequest request) {
         REGISTRY.meter(request.getRequestURI()).mark();
-        String requestBody = ((HttpRequestWrapper) request).getRequestBody();
+
+        HttpServletRequestWrapper requestWrapper = (HttpServletRequestWrapper) request;
+
+        String requestBody;
+        for (; ; ) {
+            if (requestWrapper instanceof HttpRequestWrapper) {
+                requestBody = ((HttpRequestWrapper) requestWrapper).getRequestBody();
+                break;
+            } else {
+                requestWrapper = (HttpServletRequestWrapper) requestWrapper.getRequest();
+            }
+        }
+
         LOGGER.error("Uncaught exception! uri:{}, body:{}, ", request.getRequestURL(), requestBody, ex);
         final Object unknownExceptionResult = ApiResponseCodeUtils.getUnknownExceptionResult(ex);
         return unknownExceptionResult;
