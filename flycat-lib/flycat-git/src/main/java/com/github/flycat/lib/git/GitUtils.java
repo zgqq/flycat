97c103ac5e5da5ad63deb4219a306fdedaf7d37d
zgqq <zgqjava@gmail.com>
2019-10-24 00:25:14 +0800

    feat(module): add git lib

---
+++ flycat-lib/flycat-git/src/main/java/com/github/flycat/lib/git/GitUtils.java
@@ -0,0 +1,82 @@
+package com.github.flycat.lib.git;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectReader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.treewalk.AbstractTreeIterator;
+import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.treewalk.filter.PathSuffixFilter;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+public class GitUtils {
+
+    public static AbstractTreeIterator prepareTreeParser(Repository repository, RevCommit revCommit) throws IOException {
+        return prepareTreeParser(repository, revCommit.getId());
+    }
+
+    public static AbstractTreeIterator prepareTreeParser(Repository repository, ObjectId objectId) throws IOException {
+        // from the commit we can build the tree which allows us to construct the TreeParser
+        //noinspection Duplicates
+        try (RevWalk walk = new RevWalk(repository)) {
+            RevCommit commit = walk.parseCommit(objectId);
+            RevTree tree = walk.parseTree(commit.getTree().getId());
+
+            CanonicalTreeParser treeParser = new CanonicalTreeParser();
+            try (ObjectReader reader = repository.newObjectReader()) {
+                treeParser.reset(reader, tree.getId());
+            }
+
+            walk.dispose();
+
+            return treeParser;
+        }
+    }
+
+    public static Git cloneOrPullRepo(String uri, File repoDir) throws GitAPIException, IOException {
+        Git git;
+        try {
+            git = Git.cloneRepository()
+                    .setURI(uri)
+                    .setDirectory(
+                            repoDir).call();
+        } catch (JGitInternalException e) {
+            git = Git.open(repoDir);
+            git.pull().call();
+        }
+        return git;
+    }
+
+    public static List<DiffEntry> getDiffEntries(
+            Git git,
+            AbstractTreeIterator commit1,
+            AbstractTreeIterator commit2,
+            String suffix
+    ) throws GitAPIException {
+        List<DiffEntry> diff = null;
+        if (commit1 != null) {
+            if (commit2 != null) {
+                diff = git.diff().
+                        setOldTree(commit2).
+                        setNewTree(commit1).
+                        setPathFilter(PathSuffixFilter.create(suffix)).
+                        call();
+            } else {
+                diff = git.diff().
+                        setNewTree(commit1).
+                        setPathFilter(PathSuffixFilter.create(suffix)).
+                        call();
+            }
+        }
+        return diff;
+    }
+}
