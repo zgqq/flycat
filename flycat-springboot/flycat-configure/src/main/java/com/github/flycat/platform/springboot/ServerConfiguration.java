555072189ab921ad5dcef5797d3ce4e729c40ef7
zgqq <zgqjava@gmail.com>
2019-11-23 23:10:31 +0800

    refactor(eventbus): async find listener

--- flycat-springboot/flycat-configure/src/main/java/com/github/flycat/platform/springboot/ServerConfiguration.java
+++ flycat-springboot/flycat-configure/src/main/java/com/github/flycat/platform/springboot/ServerConfiguration.java
@@ -22,11 +22,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Method;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 @Configuration
 public class ServerConfiguration {
@@ -36,39 +40,54 @@ public class ServerConfiguration {
         return new EventBusPostProcessor();
     }
 
-    public static class EventBusPostProcessor implements BeanPostProcessor {
+    public static class EventBusPostProcessor implements BeanPostProcessor, ApplicationListener<ContextRefreshedEvent> {
         private final Logger log = LoggerFactory.getLogger(this.getClass());
+        private Set<Object> listeners = Collections.synchronizedSet(new HashSet<>());
+        private List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
 
         @Override
         public Object postProcessBeforeInitialization(Object bean, String beanName)
                 throws BeansException {
+            return checkAndAddListener(bean, beanName);
+        }
+
+        private Object checkAndAddListener(Object bean, String beanName) {
+//            CompletableFuture<Void> completableFuture = CompletableFuture.runAsync(() -> {
+                // for each method in the bean
+                Method[] methods = bean.getClass().getMethods();
+                for (Method method : methods) {
+                    // check the annotations on that method
+                    Annotation[] annotations = method.getAnnotations();
+                    for (Annotation annotation : annotations) {
+                        // if it contains the Subscribe annotation
+                        if (annotation.annotationType().equals(Subscribe.class)) {
+                            // register it with the event bus
+//                        registerListener(bean, beanName, method);
+                            infoListener(bean, beanName, method);
+                            listeners.add(bean);
+                        }
+                    }
+                }
+//            });
+//            completableFutures.add(completableFuture);
             return bean;
         }
 
+        private void infoListener(Object bean, String beanName, Method method) {
+            log.trace("Bean {} containing method {} was subscribed to {}",
+                    new Object[]{
+                            beanName, method.getName(),
+                            EventBus.class.getCanonicalName()
+                    });
+            // we only need to register once
+        }
+
         @Override
-        public Object postProcessAfterInitialization(Object bean, String beanName)
-                throws BeansException {
-            // for each method in the bean
-            Method[] methods = bean.getClass().getMethods();
-            for (Method method : methods) {
-                // check the annotations on that method
-                Annotation[] annotations = method.getAnnotations();
-                for (Annotation annotation : annotations) {
-                    // if it contains the Subscribe annotation
-                    if (annotation.annotationType().equals(Subscribe.class)) {
-                        // register it with the event bus
-                        EventManager.register(bean);
-                        log.trace("Bean {} containing method {} was subscribed to {}",
-                                new Object[]{
-                                        beanName, method.getName(),
-                                        EventBus.class.getCanonicalName()
-                                });
-                        // we only need to register once
-                        return bean;
-                    }
-                }
+        public void onApplicationEvent(ContextRefreshedEvent event) {
+            CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[]{})).join();
+            for (Object listener : listeners) {
+                EventManager.register(listener);
             }
-            return bean;
         }
     }
 }
