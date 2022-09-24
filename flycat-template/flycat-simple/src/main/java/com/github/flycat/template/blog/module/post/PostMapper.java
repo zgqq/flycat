eae3f34e654606fdd891654d2bba838453d95fd9
zgqq <zgqjava@gmail.com>
2022-09-24 16:54:34 +0800

    Remove template projects

--- flycat-template/flycat-simple/src/main/java/com/github/flycat/template/blog/module/post/PostMapper.java
+++
@@ -1,9 +0,0 @@
-package com.github.flycat.template.blog.module.post;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PostMapper extends BaseMapper<Post>  {
-
-}
