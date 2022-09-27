23b7f60e9427b227404409ba90089305d921b9bb
zgqq <zgqjava@gmail.com>
2022-09-27 18:19:30 +0800

    Add deployment information

--- flycat-springboot/flycat-app/src/main/java/com/github/flycat/starter/app/context/ServerContextListener.java
+++ flycat-springboot/flycat-app/src/main/java/com/github/flycat/starter/app/context/ServerContextListener.java
@@ -22,6 +22,9 @@ import com.google.common.base.Stopwatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Map;
+import java.util.Set;
+
 public class ServerContextListener implements ContextListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(ServerContextListener.class);
     private volatile Stopwatch stopwatch;
@@ -38,14 +41,29 @@ public class ServerContextListener implements ContextListener {
         String currentProfile = ContextUtils.getCurrentProfile();
         LOGGER.info("Trying to notify starting, notifyEnabled:{}, inEnv:{}, currentProfile:{}, defaultProductions:{}," +
                 " configs:{}", booleanValue, inEnv, currentProfile, defaultProductionEnvString, context);
+
+        String appVersion = System.getProperty("app.version");
+        String gitDiff = System.getProperty("app.git.diff");
+        StringBuilder deployInfo = getDeployInfo();
+        String message = "Server[" + contextFreeConfiguration.getApplicationName() + "] starting\n" +
+                "Version: " + appVersion + "\n" + gitDiff + "\n"+ deployInfo;
+        LOGGER.info(message);
         if (booleanValue && inEnv) {
-            String appVersion = System.getProperty("app.version");
-            String gitDiff = System.getProperty("app.git.diff");
-            String message = "Server[" + contextFreeConfiguration.getApplicationName() + "] starting\n" +
-                    "Version: " + appVersion + "\n" + gitDiff;
-            LOGGER.info(message);
             NotifierUtils.sendNotification(message);
         }
+    }
+
+    private static StringBuilder getDeployInfo() {
+        Map<String, String> env = System.getenv();
+        Set<Map.Entry<String, String>> entries = env.entrySet();
+        StringBuilder deployInfo = new StringBuilder("Deploy environment: \n");
+        for (Map.Entry<String, String> entry : entries) {
+            if (entry.getKey().startsWith("DEPLOY")) {
+                deployInfo.append(entry.getKey() + "=" + entry.getValue());
+                deployInfo.append("\n");
+            }
+        }
+        return deployInfo.delete(deployInfo.length() - 2, deployInfo.length());
     }
 
     @Override
@@ -59,20 +77,21 @@ public class ServerContextListener implements ContextListener {
         boolean inEnv = ContextUtils.currentEnvIn(ContextUtils.parseEnvString(context));
         LOGGER.info("Trying to notify started, notifyEnabled:{}, inEnv:{}, currentProfile:{}, defaultProductions:{}," +
                 " configs:{}", booleanValue, inEnv, currentProfile, defaultProductionEnvString, context);
+
+        String appVersion = System.getProperty("app.version");
+        String gitDiff = System.getProperty("app.git.diff");
+        String applicationName = applicationConfiguration.getApplicationName();
+        String message = "Server[" + applicationName + "] started";
+
+        if (stopwatch != null) {
+            stopwatch.stop();
+            message = message + "\nStartup cost: " + stopwatch;
+        }
+
+        StringBuilder deployInfo = getDeployInfo();
+        message = message + "\nVersion: " + appVersion + "\n" + gitDiff + "\n" + deployInfo;
+        LOGGER.info(message);
         if (booleanValue && inEnv) {
-            String appVersion = System.getProperty("app.version");
-            String gitDiff = System.getProperty("app.git.diff");
-            String applicationName = applicationConfiguration.getApplicationName();
-            String message = "Server[" + applicationName + "] started";
-
-            if (stopwatch != null) {
-                stopwatch.stop();
-                message = message + "\nStartup cost: " + stopwatch;
-            }
-
-            message = message + "\nVersion: " + appVersion + "\n" + gitDiff;
-
-            LOGGER.info(message);
             NotifierUtils.sendNotification(message);
         }
     }
