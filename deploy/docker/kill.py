6b465e43af0dce3ee934e2e3f6ef77c345da960c
zgqq <zgqjava@gmail.com>
2022-10-12 18:29:15 +0800

    Add kill all containers

---
+++ deploy/docker/kill.py
@@ -0,0 +1,19 @@
+from conf import *
+from subprocess import check_output
+
+def execute(cmd):
+    print('Executing command %s' % (cmd))
+    return check_output(cmd, shell=True).decode().strip()
+
+app_id = check_output('docker ps -f name='+APP_NAME+' -q', shell=True).decode().strip()
+if app_id:
+   execute('docker stop ' + app_id)
+
+sba_id = check_output('docker ps -f name=web-sba -q', shell=True).decode().strip()
+if sba_id:
+   execute('docker stop ' + sba_id)
+
+web_traefik_id = check_output('docker ps -f name=web_traefik -q', shell=True).decode().strip()
+if web_traefik_id:
+   execute('docker stop ' + web_traefik_id)
+
